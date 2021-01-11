25fe5368cf4b2e7b8509460972092d510be36dbb
idraveUPF@git.com <ivandaniel.rodriguez01@estudiant.upf.edu>
2021-01-11 12:05:42 +0100

    Added graph (node, edges) encoding option to parser

--- fondpddl/encoding.py
+++ fondpddl/encoding.py
@@ -2,6 +2,25 @@ from fondpddl import Problem
 from fondpddl.algorithm import GraphIterator
 from fondpddl.utils import Index
 from fcfond.names import STATE_N, ACTION_N
+import clingo
+
+def node_1(node):
+    return clingo.Function('node', [node])
+
+def edge_1(node_1, node_2):
+    return clingo.Function('edge', [clingo.Function('', [node_1, node_2])])
+
+def tlabel_2(node_1, node_2, label):
+    return clingo.Function('tlabel', [clingo.Function('', [node_1, node_2]), label])
+
+def action_1(action):
+    return clingo.Function('action', [action])
+
+def id_2(symbol, id):
+    return clingo.Function('id', [symbol, id])
+
+def root_1(node):
+    return clingo.Function('root', [node])
 
 def clingo_problem_encoding(problem: Problem, iterator: GraphIterator,
                             expand_goal=True, ids=True, log=False,
@@ -30,6 +49,40 @@ def clingo_problem_encoding(problem: Problem, iterator: GraphIterator,
     if logdict != None:
         logdict[STATE_N] = len(state_index)
         logdict[ACTION_N] = len(action_index)
+
+    if log:
+        print('States:')
+        for i, elem in enumerate(state_index):
+            print(f'State {i}: ', elem.string(problem))
+        for i, elem in enumerate(action_index):
+            print(f'Action {i}: ', str(elem))
+
+    return
+
+def clingo_problem_graph(problem: Problem, iterator: GraphIterator,
+                            expand_goal=True, ids=True, log=False,
+                            logdict=None):
+    state_index = Index()
+    action_index = Index()
+    first = True
+
+    for state in iterator.iterate(problem, expand_goal=expand_goal):
+        st = state_index.get_index(state)
+        yield node_1(st)
+        if first:
+            yield root_1(st)
+            first = False
+        print('node ', len(state_index), end='\r')
+        for action, children in state.transitions:
+            assert len(children) == 1 # deterministic domains
+            child = state_index.get_index(children[0])
+            yield edge_1(st, child)
+            yield tlabel_2(st, child, action_index.get_index(action.action.name))
+        if ids:
+            yield id_2(node_1(state.string(problem)), state_index.get_index(state))
+    if ids:
+        for action in action_index:
+            yield id_2(action_1(action), action_index.get_index(action))
 
     if log:
         print('States:')
--- fondpddl/main.py
+++ fondpddl/main.py
@@ -1,4 +1,4 @@
-from fondpddl import encode_clingo_problem
+from fondpddl import encode_clingo_problem, get_labeled_graph
 from fondpddl.algorithm import BreadthFirstSearch
 from argparse import ArgumentParser
 import cProfile, pstats, io
@@ -7,12 +7,16 @@ import time
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('domain')
-    parser.add_argument('problem')
-    parser.add_argument('-log', action='store_true')
-    parser.add_argument('-profile', action='store_true')
-    parser.add_argument('-expgoal', action='store_true')
+    parser.add_argument('domain', help='Domain PDDL file')
+    parser.add_argument('problem', help='Problem PDDL file')
+    parser.add_argument('output', help='Output file for clingo symbols')
+    parser.add_argument('-log', action='store_true', help='Display extra information')
+    parser.add_argument('-profile', action='store_true', help='Perform profiling (does not produce output file)')
+    parser.add_argument('-expgoal', action='store_true', help='Whether to expand goal states')
+    parser.add_argument('-graph', action='store_true', help='Output symbols as nodes and edges in labeled graph')
     args = parser.parse_args()
+    if args.graph and args.expgoal:
+        raise Warning('Argument \'-expgoal\' not used in option \'-graph\', which expands complete state graph')
     if args.profile:
         pr = cProfile.Profile()
         pr.enable()
@@ -26,7 +30,12 @@ if __name__ == "__main__":
         ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
         ps.print_stats()
         print(s.getvalue())
+    elif args.graph:
+        with open(args.output, 'w') as fp:
+            for sym in get_labeled_graph(args.domain, args.problem, iterator=BreadthFirstSearch(), log=args.log):
+                fp.write(str(sym)+'.\n')
     else:
-        for sym in encode_clingo_problem(args.domain, args.problem, iterator=BreadthFirstSearch(), log=args.log, expand_goal=args.expgoal):
-            #print(str(sym)+'.')
-            pass
+        with open(args.output, 'w') as fp:
+            for sym in encode_clingo_problem(args.domain, args.problem, iterator=BreadthFirstSearch(), log=args.log, expand_goal=args.expgoal):
+                fp.write(str(sym)+'.\n')
+            

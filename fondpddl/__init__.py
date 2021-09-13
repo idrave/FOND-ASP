ab429ad82e3ceb242a04eccc5d390a8d1afc765a
ivan <ivandanielra@gmail.com>
2021-09-13 15:14:04 +0200

    Remaining changes to logging

--- fondpddl/__init__.py
+++ fondpddl/__init__.py
@@ -10,6 +10,9 @@ import fondpddl.condition
 import fondpddl.effect
 import fondpddl.algorithm
 import fondpddl.encoding
+import logging
+
+logger = logging.getLogger(__name__)
 
 def load_domain_and_problem(domain_file, problem_file):
     domain = Domain.parse(domain_file)
@@ -17,23 +20,21 @@ def load_domain_and_problem(domain_file, problem_file):
     return problem
 
 def encode_clingo_problem(domain_file, problem_file, iterator=None,
-                          expand_goal=False, log=False, track=True, logdict=None):
+                          expand_goal=False, track=True, logdict=None):
     iterator = iterator if iterator != None else fondpddl.algorithm.BreadthFirstSearch()
     problem = load_domain_and_problem(domain_file, problem_file)
-    if log:
-        print(str(problem.domain))
-        print(str(problem))
+    logger.debug(str(problem.domain))
+    logger.debug(str(problem))
     symbols = fondpddl.encoding.clingo_problem_encoding(
-                problem, iterator, expand_goal=expand_goal, log=log, track=track, logdict=logdict)
+                problem, iterator, expand_goal=expand_goal, track=track, logdict=logdict)
     return symbols
 
 def get_labeled_graph(domain_file, problem_file, iterator=None,
-                        expand_goal=True, log=False, logdict=None):
+                        expand_goal=True, track=True, logdict=None):
     iterator = iterator if iterator != None else fondpddl.algorithm.BreadthFirstSearch()
     problem = load_domain_and_problem(domain_file, problem_file)
-    if log:
-        print(str(problem.domain))
-        print(str(problem))
+    logger.debug(str(problem.domain))
+    logger.debug(str(problem))
     symbols = fondpddl.encoding.clingo_problem_graph(
-                problem, iterator, expand_goal=expand_goal, log=log, logdict=logdict)
+                problem, iterator, expand_goal=expand_goal, logdict=logdict)
     return symbols
